4b2ac105d2d152e62726341fd8487cbb4eaa6bd5
lorenz1493 <37382065+lorenz1493@users.noreply.github.com>
2018-07-18 18:23:10 -0300

    Add files via upload

--- Ejercicio1.m
+++ Ejercicio1.m
@@ -1,10 +1,18 @@
-% Mi RUT es (utilizo parametros ppt, por error que genera mi rut)
-
-A=1 ; B=2 ; C=3 ; D=4 ; E=5 ; F=6 ; G=7 ; H=8 ; I=0 ;
-
-t = 0 : 0.3 : 30 ; v = exp(-((B+A)/(2*C*(D+E))) * t) .* sin(t);
-
-subplot 121; plot(t,v); title('PLOT');
-xlabel('Tiempo [seg]'); ylabel('Volts'); grid;
-subplot 122; stem(t,v); title('STEM');
-xlabel('Tiempo [seg]'); ylabel('Volts'); grid;
+h1 = [0.05 0.45 0.56 0.44 0.05];
+h2 = [3.2 0.25 0.4 0.6 0.25]; 
+h1 = h1 / (sum(h1));
+h2 = h2 / (sum(h2)); 
+[H1, w1] = freqz(h1, 1);
+n1 = [0 : 0.1 : 3.9] * pi; x1 = square(n1); 
+y1 = conv(x1, h1); 
+subplot 221; stem(x1); title('x1 (entrada)');
+axis([0 45 -1.5 1.5]); xlabel('n'); grid
+subplot 222; stem(y1); title('y1 (salida)');
+axis([0 45 -1.5 1.5]); xlabel('n'); grid
+[H2, w2] = freqz(h2, 1);
+n2 = [0 : 0.1 : 3.9] * pi; x2 = square(n2); 
+y2 = conv(x2, h2);
+subplot 223; stem(x2); title('x2 (entrada)');
+axis([0 45 -1.5 1.5]); xlabel('n'); grid
+subplot 224; stem(y2); title('y2 (salida)');
+axis([0 45 -1.5 1.5]); xlabel('n'); grid
